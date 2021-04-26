00cd01f11e532666a9699221ba8b080922422300
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-04-26 14:01:27 +0200

    Update task_0_6.py

--- task_0_6.py
+++ task_0_6.py
@@ -1,19 +1,3 @@
-def find_biggest_num(num1,num2,num3):
-    
-    biggest_number=0
-    if num1>num2 and num1>num3:
-        biggest_number=num1
-    elif num2>num1 and num2>num3:
-        biggest_number=num2
-    else :
-        biggest_number=num3
-    
-    print(biggest_number)
-
-find_biggest_num(15,4,1)
-    
-#updated function
-
 def find_biggest_num2(*args):
     
     biggest_number=0
@@ -25,4 +9,4 @@ def find_biggest_num2(*args):
     print(biggest_number)
 
 
-find_biggest_num2(1,2,3,4,5,6,7,8)
+find_biggest_num2(1,2,3,4,5,6)
