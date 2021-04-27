35954d0179a1b96d320fb7db404eaa1bc296ede1
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-04-27 21:50:06 +0200

    Update task_0_9.py

--- task_0_9.py
+++ task_0_9.py
@@ -3,12 +3,13 @@ def print_vowel(sentence):
     lowercase = set(sentence.lower()) 
     vowels_in_sentence=[]
     vowel=["a","e","i","o","u"]
+    string_vowels = ""
     for x in lowercase:
         for y in vowel:
             if x==y: 
                 vowels_in_sentence.append(x)
     
-    print("Vowels:"+str(vowels_in_sentence))
+    return("Vowels: "+ string_vowels+ ", ".join(vowels_in_sentence))
 
 
-print_vowel('LUNGILE MINENHLE NJAKAZI')
+print(print_vowel('LUNGILE MINENHLE NJAKAZI'))
