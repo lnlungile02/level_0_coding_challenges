ed8caf049c74567b86bde8225776f7cc9caa87b7
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-04-28 12:15:35 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -1,11 +1,11 @@
 def convert_to_time(number):
 
   if number < 60:
-    print ("0:" + str(number))
+    return "0:" + str(number)
   else:
     number = number % (24*3600)
     hours = number // 60
     number %= 60
-    print (str(hours) + ":" + str(number))
+    return str(hours) + ":" + str(number)
  
-convert_to_time(126)
+print(convert_to_time(11))
