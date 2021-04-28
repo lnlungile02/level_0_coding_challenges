a76d3ce49fb746ea56bbd6ccc2594db5981e04e9
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-04-28 12:04:45 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -3,7 +3,7 @@ def triangle_area_calc(side_a,side_b,side_c):
     semi = (side_a + side_b + side_c)/2
     area = (semi* (semi - side_a)* (semi - side_b)* (semi - side_c))**0.5
 
-    print(str(round(area,2)))
+    return str(round(area,2))
 
 
-triangle_area_calc(float(4),float(6),float(8))
+print(triangle_area_calc(float(4),float(6),float(8)))
