38ee9b1170b9343f1664f0788a65d930b819d512
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-04-28 12:13:23 +0200

    Update task_0_7.py

--- task_0_7.py
+++ task_0_7.py
@@ -2,10 +2,10 @@ def convert_to__celciius(f):
     celcius=(f-32)*(5/9)
     return celcius
 
-convert_to__celciius(92)
-
 def convert_to_fahrenheit(c):
     fahrenheit = (c*(9/5))+32
     return fahrenheit
 
-convert_to_fahrenheit(33)
+print(convert_to__celciius(92))
+
+print(convert_to_fahrenheit(33))
