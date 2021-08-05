9453cd6e149eee47110580748a18103f561f4850
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-08-05 21:01:26 +0200

    Update task_0_2.py

--- task_0_2.py
+++ task_0_2.py
@@ -1,11 +1,12 @@
-x = 1+1*2
-y= (1+1)*2
-z = 1+(1*2)
-a = 1+1*2/2
-b= (1+1*2)/2
+x = 1 + 1 * 2
+y = (1 + 1) * 2
+z = 1 + (1 * 2)
+a = 1 + 1 * 2 / 2
+b = (1 + 1 * 2) / 2
 
-print (x)
-print (y)
-print (z)
-print (a)
-print (b)
+if __name__ in "__main__":
+    print(x)
+    print(y)
+    print(z)
+    print(a)
+    print(b)
