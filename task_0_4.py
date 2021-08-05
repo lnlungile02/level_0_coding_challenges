a2de4592c50300d27fba911d1bc0db3fe214c4f8
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-08-05 21:42:55 +0200

    Update task_0_4.py

--- task_0_4.py
+++ task_0_4.py
@@ -1,7 +1,13 @@
-def even_or_odd (number):
-    if number%2==0 :
-        return "even"
-    elif number%2==1:
-        return "odd"
+def even_or_odd(number):
+    if number % 2 == 0:
+        print("even")
+    elif number % 2 == 1:
+        print("odd")
 
-print(even_or_odd(16))
+
+def main():
+    even_or_odd(16)
+
+
+if __name__ in "__main__":
+    main()
