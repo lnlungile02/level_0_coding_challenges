c498965fe6735b9be54f8803d1be84ca67c5747a
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-08-06 11:18:28 +0200

    Update task_0_10.py

--- task_0_10.py
+++ task_0_10.py
@@ -1,15 +1,19 @@
-def print_vowel(sentence1,sentence2):
-    similar_letters=[]
+def print_vowel(sentence1, sentence2):
+    similar_letters = []
     lowercase1 = list(sentence1.lower())
     lowercase2 = list(sentence2.lower())
 
     for x in lowercase1:
         for y in lowercase2:
-            if x==y:            #comapre index of strings
+            if x == y:  # comapre index of strings
                 similar_letters.append(x)
 
-    return ("Common letters: " + ", " .join(similar_letters))
+    print("Common letters: " + ", ".join(similar_letters))
 
-  
 
-print(print_vowel('house','compputers'))
+def main():
+    print_vowel("house", "computers")
+
+
+if __name__ in "__main__":
+    main()
