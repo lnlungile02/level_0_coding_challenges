936387bb89b66ac115215e17cdf05bf00ab4ea86
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-08-06 11:12:36 +0200

    Update task_0_9.py

--- task_0_9.py
+++ task_0_9.py
@@ -1,18 +1,20 @@
 def print_vowel(sentence):
 
-    lowercase = set(sentence.lower()) 
-    vowels_in_sentence=[]
-    vowel=["a","e","i","o","u"]
+    lowercase = set(sentence.lower())
+    vowels_in_sentence = []
+    vowel = ["a", "e", "i", "o", "u"]
     string_vowels = ""
     for x in lowercase:
         for y in vowel:
-            if x==y: 
+            if x == y:
                 vowels_in_sentence.append(x)
-    
-    return("Vowels: "+ string_vowels+ ", ".join(vowels_in_sentence))
+    sentence = string_vowels + ", ".join(vowels_in_sentence)
+    return f"Vowels: {sentence}"
+
 
 def main():
-    print(print_vowel('Eckard Berry'))
+    print(print_vowel("Eckard Berry"))
+
 
 if __name__ in "__main__":
     main()
