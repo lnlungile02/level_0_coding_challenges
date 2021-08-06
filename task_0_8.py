f4e9bc8e20199d412a7d771ab3430c8fda0e666c
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-08-06 10:56:37 +0200

    Update task_0_8.py

--- task_0_8.py
+++ task_0_8.py
@@ -1,11 +1,25 @@
 def convert_to_time(number):
+    sentence_hours = "hours"
+    sentence_minutes = "minutes"
+    if number < 60:
+        if number == 1:
+            sentence_minutes = "minute"
+        return f"{number} {sentence_minutes}."
+    else:
+        hours = number // 60
+        if hours == 1:
+            sentence_hours = "hour"
 
-  if number < 60:
-    return "0 " +"hours,"+ str(number) + " minutes"
-  else:
-    number = number % (24*3600)
-    hours = number // 60
-    number %= 60
-    return str(hours) +  " hours, " + str(number) + " minutes"
- 
-print(convert_to_time(11))
+        minutes = number % 60
+        if minutes == 1:
+            sentence_minutes = "minute"
+
+    return f"{hours} {sentence_hours}, {minutes} {sentence_minutes}."
+
+
+def main():
+    print(convert_to_time(61))
+
+
+if __name__ in "__main__":
+    main()
