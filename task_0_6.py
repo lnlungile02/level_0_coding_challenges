b7a7a1bfd9b89a00f72773fdfd1c88c2ee1b0a96
lnlungile02 <76539464+lnlungile02@users.noreply.github.com>
2021-08-10 11:38:28 +0200

    Update task_0_6.py

--- task_0_6.py
+++ task_0_6.py
@@ -1,9 +1,15 @@
 def find_biggest_num2(*args):
-    return max(args)
+    maximum_number = 0
+
+    for number in args:
+        if number > maximum_number:
+            maximum_number = number
+
+    return maximum_number
 
 
 def main():
-    print(find_biggest_num2(1, 2, 3, 4, 5, 6))
+    print(find_biggest_num2(1, 2, 3, 9, 5, 6))
 
 
 if __name__ in "__main__":
